0460658c7bb9a24789e120021ccb31d2e9a4d5d0
Brooke Frandsen <brooke@churchofjesuschrist.org>
2020-10-22 17:52:17 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const app = express()
+const path = require("path");
+
+// This game lives at the root
+app.get("/", function (req, res) {
+    res.sendFile(__dirname+"/index.html")
+})
+
+// Static files
+app.use('/lib', express.static(path.join(__dirname, 'static')))
+   
+const server = app.listen(1234, function () {
+    let host = server.address().address || 'localhost'
+    const port = server.address().port
+
+    if(host === '::') {
+        host = 'localhost'
+    }
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ static/yahtzee.js
@@ -0,0 +1,19 @@
+
+const randomNumber = function(min, max) {
+    
+};
+
+const rollDice = function() {
+
+}
+
+window.addEventListener('load', function () {
+
+    const rollButton = document.getElementById('rollButon');
+    
+    rollButton.addEventListener('click', function (event) {
+        const allDice = [].slice.call(document.querySelectorAll("[id^='dice']"))
+        console.log('roll clicked')
+    });
+
+});
